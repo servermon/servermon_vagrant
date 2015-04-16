08e2b906d638954eaba86d3a32f38fa4da99c9da
Alexandros Kosiaris <akosiaris@gmail.com>
2015-04-16 12:03:39 +0300

    Move the puppet models management to django.sh
    It anyway makes more sense to have it in there

--- django.sh
+++ django.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 cd /srv/servermon/servermon
+sed -i -e 's/managed = False/managed = True/' puppet/models.py
 ./manage.py syncdb --migrate --noinput
+# And now undo it
+sed -i -e 's/managed = True/managed = False/' puppet/models.py
--- git.sh
+++ git.sh
@@ -6,5 +6,4 @@ apt-get install git -y
 cd /srv/
 if [ ! -d servermon ]; then
 	git clone http://github.com/servermon/servermon.git
-	sed -i -e 's/managed = False/managed = True/' servermon/servermon/puppet/models.py
 fi

6d50388549ee43a5a31eefef1cdb4b7fb44cba6f
Alexandros Kosiaris <akosiaris@gmail.com>
2015-09-22 19:03:30 +0300

    Populate the DB with sample data
    Populate the DB with sample data generated via the dev_fixtures command.
    To make that possible also load vendor models before that

--- django.sh
+++ django.sh
@@ -4,5 +4,7 @@ cd /srv/servermon/servermon
 sed -i -e 's/managed = False/managed = True/' puppet/models.py
 ./manage.py syncdb --noinput
 ./manage.py migrate --noinput
+./manage.py loaddata vendor-model
+./manage.py loaddata sampledata
 # And now undo it
 sed -i -e 's/managed = True/managed = False/' puppet/models.py

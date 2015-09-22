a42a382cd62e0f6ac6fa608cc9d5c0d2b7b11a92
Alexandros Kosiaris <akosiaris@gmail.com>
2015-09-22 19:01:07 +0300

    Use migrate command instead of --migrate to syncdb command
    In Django 1.7 syncdb command does not take any longer the --migrate
    argument. Change the django provisioner to call migrate explicitly

--- django.sh
+++ django.sh
@@ -2,6 +2,7 @@
 
 cd /srv/servermon/servermon
 sed -i -e 's/managed = False/managed = True/' puppet/models.py
-./manage.py syncdb --migrate --noinput
+./manage.py syncdb --noinput
+./manage.py migrate --noinput
 # And now undo it
 sed -i -e 's/managed = True/managed = False/' puppet/models.py
